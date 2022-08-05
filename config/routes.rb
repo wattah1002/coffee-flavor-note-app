48ea5956e80e67a8f1f5dd69092704dbcffeef3f
wattah1002 <wattah.uec18@gmail.com>
2022-08-05 20:26:56 +0900

    [#14] Static page controllerの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
   # get 'users/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get '/signup', to: 'users#new'
