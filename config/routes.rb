768387941dfbd5920d45f8108a14675206186516
wattah1002 <wattah.uec18@gmail.com>
2022-08-05 20:17:36 +0900

    [#14] Rail1s new

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  # get 'users/new'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get '/signup', to: 'users#new'
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
