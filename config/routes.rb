c83b66543d30a74ace52b029c422fd81260aa377
wattah1002 <wattah.uec18@gmail.com>
2022-08-05 20:32:20 +0900

    [#14] home route対応

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  get '/home', to: 'static_pages#home'
   # get 'users/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get '/signup', to: 'users#new'
