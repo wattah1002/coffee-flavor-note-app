8d842fff1ee61a866276f2d758ffe3f8182b5aef
wattah1002 <wattah.uec18@gmail.com>
2022-08-06 14:23:46 +0900

    [#14] バリデーションとテストの追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,53 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # let(:user) { User.new(name: 'Example User', email: 'user@example.com') }
+  let(:user) { FactoryBot.build(:user) }
+
+  it 'userが有効であること' do
+    expect(user).to be_valid
+  end
+
+  it 'nameが必須であること' do
+    user.name = ''
+    expect(user).to_not be_valid
+  end
+
+  it 'emailが必須であること' do
+    user.email = ''
+    expect(user).to_not be_valid
+  end
+
+  it 'nameは50文字以内であること' do
+    user.name = 'a' * 51
+    expect(user).to_not be_valid
+  end
+
+  it 'emailは255文字以内であること' do
+    user.email = "#{'a' * 244}@example.com"
+    expect(user).to_not be_valid
+  end
+
+  it 'emailが有効な形式であること' do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.ch]
+    valid_addresses.each do |valid_address|
+      user.email = valid_address
+      expect(user).to be_valid
+    end
+  end
+
+  it '無効な形式のemailは失敗すること' do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example. foo@bar_baz.com foo@bar+baz.com foo@bar..com]
+    invalid_addresses.each do |invalid_address|
+      user.email = invalid_address
+      expect(user).to_not be_valid
+    end
+  end
+
+  it 'emailは重複して登録できないこと' do
+    duplicate_user = user.dup
+    duplicate_user.email = user.email.upcase
+    user.save
+    expect(duplicate_user).to_not be_valid
+  end
 end
