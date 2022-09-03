bd12cc15c7c7637ac9def8083f5fc66651a56540
wattah1002 <wattah.uec18@gmail.com>
2022-09-03 11:21:36 +0900

    [#14] flashの追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "新規登録ありがとうございます！"
       redirect_to @user
     else
       render 'new'
