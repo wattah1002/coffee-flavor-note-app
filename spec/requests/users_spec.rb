cb0b35d67be85dd3435dd9a7ba123654605fc029
wattah1002 <wattah.uec18@gmail.com>
2022-09-03 11:54:21 +0900

    [#14] ユーザー登録が正常に行われた場合のテストを追加

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -23,5 +23,24 @@ RSpec.describe "Users", type: :request do
                                            password_confirmation: 'bar' } }
         }.to_not change(User, :count)
     end
+
+    context '有効な値の場合' do
+      let(:user_params) {{ user: { name: 'Example User',
+                                   email: 'user@example.com',
+                                   password: 'password',
+                                   password_confirmation: 'password' } } }
+
+      it '登録されること' do
+        expect {
+          post users_path, params: user_params
+        }.to change(User, :count).by 1
+      end
+
+      it 'users/show にリダイレクトされること' do
+        post users_path, params: user_params
+        user = User.last
+        expect(response).to redirect_to user
+      end
+    end
   end
 end
